5aeba4284db8e07a4d6d72ed6b2e133e07f3cb76
13angs <13angz.um@gmail.com>
2023-01-21 13:33:48 +0000

    create minimal web api

---
+++ backend/ChatHub.cs
@@ -0,0 +1,18 @@
+
+using Microsoft.AspNetCore.SignalR;
+
+namespace backend {
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task SendNotification(string user, string message)
+        {
+            await Clients.User(user).SendAsync("ReceiveNotification", message);
+        }
+    }
+
+}
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,21 @@
+// configure the SignalR in the startup class
+
+using backend;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
+
 var app = builder.Build();
 
+app.UseRouting();
+
+// using the UseEndpoints extension method to map the ChatHub to the /chat 
+app.UseEndpoints(endpoints => {
+    endpoints.MapHub<ChatHub>("/chat");
+});
+
 app.MapGet("/", () => "Hello World!");
+
 
 app.Run();
