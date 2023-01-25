b93c927ae0fec51b55105d96f33195f19ef0dd92
13angs <13angz.um@gmail.com>
2023-01-25 00:13:02 +0000

    display message base on user

--- README.md
+++ README.md
@@ -19,3 +19,48 @@ docker-compose up
 4. try typing on one of theme
 
 5. see if the text exist on both tab or not
+
+## To develop the project
+
+- remove the previous devcontainer (if exist)
+- open the folder in container
+
+### Dotnet
+
+- cd into `backend` and build the project
+
+```bash
+dotnet build
+```
+
+- try running the project
+
+```bash
+dotnet run
+```
+
+- continue developing
+
+### React
+
+- cd into `frontend`
+- install the packages
+
+```bash
+npm i
+```
+
+- copy and rename `.env.sample` to `.env`
+- update the `.env`
+
+```bash
+REACT_APP_API_URL=<backend_url> # http://localhost:5245
+```
+
+- run the project
+
+```bash
+npm start
+```
+
+
--- frontend/src/pages/chat/chat.page.js
+++ frontend/src/pages/chat/chat.page.js
@@ -1,18 +1,66 @@
 import React, { useState } from 'react';
 import './chat.page.css';
 import Axios from '../../backend/Axios';
-import {UserAPI} from '../../backend/api';
+import { UserAPI } from '../../backend/api';
 import { HubConnectionBuilder, HttpTransportType } from '@microsoft/signalr';
-import {useParams} from 'react-router-dom';
+import { useParams, useNavigate, useSearchParams } from 'react-router-dom';
+import routes from '../../routes/routes';
 
-const ChatPage = ({userId}) => {
+
+// get and display the user list
+function UserList({from}) {
+  const [users, setUsers] = useState([]);
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    const fetchUsers = async (url) => {
+      try {
+        const res = await Axios.get(url);
+        if (res.status === 200) {
+          const data = res.data;
+          setUsers(data);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchUsers('/api/v1/users');
+  }, [])
+
+  // when click the user push param as to=<user_id>
+  const handleUserClick = (userId) => {
+    navigate({
+      pathname: routes.chat.path.replace(':id', from), 
+      search: `?${routes.chat.search.to}=${userId}`});
+  }
+
+  return (
+    <div className="user-list-container">
+      <h3>Users</h3>
+      <div className="user-list">
+        {users.length > 0 && users.map((user) => (
+          <div key={user.id} className="user" onClick={() => handleUserClick(user.id)}>
+            <img src={'https://via.placeholder.com/50x50'} alt={user.name} className="avatar" />
+            <div className="user-name">
+              <p>{user.name}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+// get and display the chat messages
+function ChatMessage({userId}) {
+  const searchParams = useSearchParams();
+  const allParams = searchParams[0];
   const [messages, setMessages] = useState([]);
-  const {id} = useParams();
+  const [currentUser, setCurrentUser] = useState(null);
+
   const [currentMessage, setCurrentMessage] = useState('');
   const [connection, setConnection] = React.useState(null);
-
-  const [users, setUsers] = useState([]);
-  const [currentUser, setCurrentUser] = useState(null);
 
   const apiUrl = process.env.REACT_APP_API_URL;
   const messUrl = `${apiUrl}/api/v1/messages`;
@@ -33,25 +81,12 @@ const ChatPage = ({userId}) => {
 
     const fetchUser = () => {
       const user = new UserAPI();
-      user.getUser(id, setCurrentUser)
-    }
-
-    const fetchUsers = async (url) => {
-      try {
-        const res = await Axios.get(url);
-        if (res.status === 200) {
-          const data = res.data;
-          setUsers(data);
-        }
-      } catch (err) {
-        console.log(err);
-      }
+      user.getUser(userId, setCurrentUser)
     }
 
     fetchMessage('/api/v1/messages');
-    fetchUsers('/api/v1/users');
     fetchUser();
-  }, [messUrl, id])
+  }, [messUrl, userId])
 
   React.useEffect(() => {
     const fetchMessage = async (url) => {
@@ -93,46 +128,39 @@ const ChatPage = ({userId}) => {
     setCurrentMessage('');
   }
 
+  return (<div className="chat-messages-container">
+    <div className="chat-header">
+      <h2>Login as {currentUser ? currentUser['name'] : "Guest"}</h2>
+    </div>
+    <div className="chat-messages">
+      {allParams.get('to') && messages.map((message) => (
+        <div key={message.id} className="message">
+          <img src={'https://via.placeholder.com/50x50'} alt={'https://via.placeholder.com/50x50'} className="avatar" />
+          <div className="message-text">
+            <p><strong>{message.from}:</strong> {message.text}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+    <form className="chat-form" onSubmit={handleSubmit}>
+      <input
+        className="chat-input"
+        type="text"
+        placeholder="Type a message"
+        value={currentMessage}
+        onChange={(event) => setCurrentMessage(event.target.value)}
+      />
+      <button className="chat-submit" type="submit">Send</button>
+    </form>
+  </div>)
+}
+
+const ChatPage = () => {
+  const { id } = useParams();
   return (
     <div className="chat-container">
-      <div className="user-list-container">
-        <h3>Users</h3>
-        <div className="user-list">
-          {users.length > 0 && users.map((user) => (
-            <div key={user.id} className="user">
-              <img src={'https://via.placeholder.com/50x50'} alt={user.name} className="avatar" />
-              <div className="user-name">
-                <p>{user.name}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className="chat-messages-container">
-        <div className="chat-header">
-          <h2>Login as {currentUser ? currentUser['name'] : "Guest"}</h2>
-        </div>
-        <div className="chat-messages">
-          {messages.map((message) => (
-            <div key={message.id} className="message">
-              <img src={'https://via.placeholder.com/50x50'} alt={'https://via.placeholder.com/50x50'} className="avatar" />
-              <div className="message-text">
-                <p><strong>{message.from}:</strong> {message.text}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-        <form className="chat-form" onSubmit={handleSubmit}>
-          <input
-            className="chat-input"
-            type="text"
-            placeholder="Type a message"
-            value={currentMessage}
-            onChange={(event) => setCurrentMessage(event.target.value)}
-          />
-          <button className="chat-submit" type="submit">Send</button>
-        </form>
-      </div>
+      <UserList from={id} />
+      <ChatMessage userId={id}/>
     </div>
   );
 }
